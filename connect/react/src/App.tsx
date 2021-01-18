52a77c447d23ab61cfc119aacf3cf9089ba6cba6
Federico Miras <miras.federico@gmail.com>
2021-01-18 16:12:57 -0300

    feat: cleanup react application and fetch connect token

--- connect/react/src/App.tsx
+++ connect/react/src/App.tsx
@@ -1,37 +1,53 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import { PluggyConnect } from 'react-pluggy-connect';
+
 import './App.css';
-import { PluggyConnectProps, PluggyConnect } from 'react-pluggy-connect';
 
-const { REACT_APP_API_KEY = '', REACT_APP_CONNECT_URL = '' } = process.env;
+/**
+ * TODO: replace this URL with your own API returning an { accessToken } object
+ * with your Pluggy connect token
+ */
+const { REACT_APP_CONNECT_TOKEN_API_URL } = process.env
 
-function App() {
+const App = () => {
+  const [token, setToken] = useState<string>();
+  const [error, setError] = useState();
   const [isConnectVisible, setIsConnectVisible] = useState(false);
   const [includeSandbox, setIncludeSandbox] = useState(false);
 
-  const pluggyConnectWidgetProps: PluggyConnectProps = {
-    url: REACT_APP_CONNECT_URL,
-    connectToken: REACT_APP_API_KEY,
-    includeSandbox,
-    onError: (error) => {
-      console.error('Whoops! Pluggy Connect error... ', error);
-    },
-    onSuccess: (data) => {
-      console.log('Yay! Pluggy connect success!', data);
-    },
-    onOpen: () => {
-      console.log('Pluggy Connect modal opened.');
-    },
-    onClose: () => {
-      console.log('Pluggy Connect modal close.');
-      setIsConnectVisible(false);
-    },
-  };
+  useEffect(() => {
+    async function fetchToken() {
+      try {
+        const response = await fetch(REACT_APP_CONNECT_TOKEN_API_URL!);
+        const {accessToken} = await response.json();
+        setToken(accessToken);
+      } catch (error) {
+        setError(error);
+      }
+    }
+
+    fetchToken();
+  }, []);
 
   const openConnectPopup = () => {
     setIsConnectVisible(true);
   };
 
-  return (
+  if (error) {
+    return (
+    <div className="App">
+          <p>There was an error</p>
+          <p>{JSON.stringify(error)}</p>
+</div>    );
+  }
+
+  return !token ? (
+    <>
+    <div className="App">
+      Loading...
+      </div>
+    </>
+  ) : (
     <div className="App">
       <header className="App-header">
         <p>Pluggy Connect Widget Example</p>
@@ -50,9 +66,9 @@ function App() {
           Connect Account
         </button>
       </div>
-      {isConnectVisible && <PluggyConnect {...pluggyConnectWidgetProps} />}
+      {isConnectVisible && <PluggyConnect connectToken={token} includeSandbox={includeSandbox} />}
     </div>
   );
-}
+};
 
 export default App;
