03d12137416aafbb19257bab8fc82691645b8ff1
NicolasMontone <montonenicolas01@gmail.com>
2023-02-09 16:08:45 -0300

    feat: allow update last connection and clean code.

--- frontend/nextjs/pages/index.tsx
+++ frontend/nextjs/pages/index.tsx
@@ -1,14 +1,18 @@
-import Head from 'next/head'
-import dynamic from 'next/dynamic'
-import { useEffect, useState } from 'react'
-import type { PluggyConnect as PluggyConnectType } from 'react-pluggy-connect'
+import Head from "next/head"
+import dynamic from "next/dynamic"
 
-import styles from '../styles/Home.module.css'
+import { useCallback, useEffect, useState } from "react"
+import type {
+  ConnectEventPayload,
+  PluggyConnect as PluggyConnectType,
+} from "react-pluggy-connect"
+import type { Item } from "pluggy-sdk"
 
-const itemIdToUpdate = process.env.NEXT_PUBLIC_PLUGGY_UPDATE_ITEM_ID
+import styles from "../styles/Home.module.css"
 
 const PluggyConnect = dynamic(
-  () => (import('react-pluggy-connect') as any).then((mod) => mod.PluggyConnect),
+  () =>
+    (import("react-pluggy-connect") as any).then((mod) => mod.PluggyConnect),
   { ssr: false }
 ) as typeof PluggyConnectType
 
@@ -16,30 +20,69 @@ export default function Home() {
   const [withSandbox, setSandbox] = useState<boolean>(false)
   const [connectToken, setConnectToken] = useState<string>()
   const [connecting, setConnecting] = useState<boolean>(false)
+  const [item, setItem] = useState<Item | null>(null)
+
+  const generateToken = useCallback(async (itemId?: string) => {
+    const response = await fetch("/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // you can pass an itemId to generate a token to update that specific item or use
+      // the same connect token that creates the item you want to update see https://docs.pluggy.ai/docs/updating-an-item
+      body: JSON.stringify({ itemId }),
+    })
+
+    const { accessToken } = await response.json()
+    setConnectToken(accessToken)
+  }, [])
+
+  const handleOpenConnectInUpdateMode = useCallback(async () => {
+    if (!item) {
+      // nothing to update -> just return
+      return
+    }
+    await generateToken(item.id)
+    setConnecting(true)
+  }, [item])
 
   useEffect(() => {
-    if (!connectToken) {
-      const fetchToken = async () => {
-        const response = await fetch('/api/token')
-        const { accessToken } = await response.json()
-        setConnectToken(accessToken)
-      }
+    // generate a token to be used in the Pluggy Connect when component is mounted
+    generateToken()
+  }, [generateToken])
 
-      fetchToken()
+  const onSuccess = useCallback((itemData: { item: Item }) => {
+    console.log("Yaaay, we got an item: ", itemData.item)
+    setItem(itemData.item)
+  }, [])
+
+  const onError = useCallback((error) => {
+    console.log("Oops, there was an error: ", error)
+  }, [])
+
+  const handleEvent = useCallback((payload: ConnectEventPayload) => {
+    const { event } = payload
+
+    console.log("[event]", event)
+  }, [])
+
+  const handleCheckboxChange = useCallback((event) => {
+    setSandbox((previous) => !previous)
+  }, [])
+
+  const handleClose = useCallback(() => {
+    setConnecting(false)
+  }, [])
+
+  const handleOpenConnectInCreateMode = useCallback(async () => {
+    // if there is an item, we remove it from the state
+    // to be able to open the Pluggy Connect widget in create mode
+    if (item) {
+      setItem(undefined)
     }
-  })
 
-  const onSuccess = (itemData) => {
-    console.log(itemData.item)
-  }
-
-  const onError = (error) => {
-    console.log('There was an error: ', error)
-  }
-
-  const handleCheckboxChange = (event) => {
-    setSandbox(!withSandbox)
-  }
+    setConnecting(true)
+  }, [item])
 
   return (
     <div className={styles.container}>
@@ -54,40 +97,55 @@ export default function Home() {
         <h1 className={styles.title}>Pluggy Connect</h1>
 
         <p className={styles.description}>
-          Quickstart application to run Pluggy Connect with{' '}
-          <a href="https://github.com/vercel/next.js" target="_blank" rel="noopener noreferrer">
+          Quickstart application to run Pluggy Connect with{" "}
+          <a
+            href="https://github.com/vercel/next.js"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Next.js
           </a>
         </p>
 
-        <div className="sandbox-checkbox">
-          <input type="checkbox" checked={withSandbox} onChange={handleCheckboxChange} />
+        <div className={styles.sandboxCheckbox}>
           <label>
+            <input
+              type="checkbox"
+              checked={withSandbox}
+              onChange={handleCheckboxChange}
+              className={styles.input}
+            />
             Include{' '}
-            <a href="https://docs.pluggy.ai/#sandbox" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://docs.pluggy.ai/#sandbox"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               sandbox connectors
             </a>
           </label>
         </div>
 
         <div className={styles.grid}>
-          {connecting ? (
+          <button onClick={handleOpenConnectInCreateMode}>
+            Connect an account
+          </button>
+          <button
+            className={`secondary ${!item ? "disabled" : ""}`}
+            onClick={item ? handleOpenConnectInUpdateMode : undefined}
+          >
+            Update last connection
+          </button>
+          {connecting && (
             <PluggyConnect
-              updateItem={itemIdToUpdate}
+              updateItem={item?.id}
               connectToken={connectToken}
               includeSandbox={withSandbox}
               onSuccess={onSuccess}
               onError={onError}
+              onClose={handleClose}
+              onEvent={handleEvent}
             />
-          ) : (
-            <button
-              className={styles.card}
-              disabled={!connectToken}
-              onClick={() => setConnecting(true)}
-            >
-              <h3>Start here</h3>
-              <p>Connect your financial account and see your report.</p>
-            </button>
           )}
         </div>
       </main>
@@ -98,7 +156,8 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by <img src="/logo.png" alt="Pluggy Logo" className={styles.logo} />
+          Powered by{" "}
+          <img src="/logo.png" alt="Pluggy Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
